ec482a27219e13d92c96d1b2083af5f44d3199e9
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-08-17 12:02:44 +0530

    Add files via upload

---
+++ TestDetailDao.java
@@ -0,0 +1,11 @@
+package org.javapatil.testdetail.dao;
+
+import java.util.List;
+
+import org.javapatil.testdetail.model.TestDetailForm;
+
+public interface TestDetailDao {
+
+	List<TestDetailForm> getTestDetailList();
+	
+}
