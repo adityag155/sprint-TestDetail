6dc2e70a7d73835ecc46a64760ef19086d6b489b
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-08-17 12:04:26 +0530

    Add files via upload

---
+++ TestDetailService.java
@@ -0,0 +1,10 @@
+package org.javapatil.testdetail.service;
+
+import java.util.List;
+
+import org.javapatil.testdetail.model.TestDetailForm;
+
+public interface TestDetailService {
+	
+	List<TestDetailForm> getTestDetailListService();
+}
