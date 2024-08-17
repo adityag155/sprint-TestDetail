8fe7c729580ff19e27d00050805cb99e9fa93a93
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-08-17 12:04:08 +0530

    Add files via upload

---
+++ TestDetailForm.java
@@ -0,0 +1,19 @@
+package org.javapatil.testdetail.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestDetailForm {
+
+	
+	private int Test_id;
+	private int Test_no;
+	private int Question_no;
+	
+}
